7e30ebba6914edadf886073d4a9ff7027d03c5ac
Владимир Алефман <smagazhevskijj@gmail.com>
2021-12-01 13:41:14 +0200

    deleted 1 loop

--- classWork.js
+++ classWork.js
@@ -11,10 +11,7 @@ const array = [
 const displayOccurences = (ar)=>{
     const res = {}
     for(let i=0; i<ar.length; i++){
-        res[ar[i]] = 0
-    }
-    for(let i=0; i<ar.length; i++){
-        res[ar[i]]++
+        res[ar[i]] = res[ar[i]]!=undefined?++res[ar[i]]:1
     }
     Object.entries(res).sort((a,b)=>b[1]-a[1]).forEach(s=>console.log(`${s[0]} - ${s[1]}`))
 }
